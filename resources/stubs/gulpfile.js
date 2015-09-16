66a3a843618a1c7912cf39aa37c847b51cabcfce
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-09-16 23:28:33 +0200

    Updated gulp to mix together the vat calculator and the main app

--- resources/stubs/gulpfile.js
+++ resources/stubs/gulpfile.js
@@ -13,5 +13,9 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss')
+        .scripts([
+            '../../../public/vat_calculator.js',
+            'app.js'
+        ], 'public/js/app.js')
        .browserify('app.js');
 });
